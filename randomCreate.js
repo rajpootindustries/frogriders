1565dd9e5f648630bf310b8b4b4877bc2a719235
sanjay <32687097+rajpootindustries@users.noreply.github.com>
2019-07-16 19:27:22 -0700

    create random fog classes for four colors and 34 frogs

---
+++ randomCreate.js
@@ -0,0 +1,17 @@
+//13 brown, 7 red, 9 blue, 5 yellow
+
+function createFrogs() {
+  var imageArray = ['yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'brown', 'brown', 'brown', 'brown', 'brown', 'brown', 'brown', 'brown', 'brown', 'brown', 'brown', 'brown', 'brown'];
+  for (var images = 0; images < 34; images++) {
+    imageIndex = Math.floor(Math.random() * imageArray.length)
+    var image = imageArray[imageIndex];
+    imageArray.splice(imageIndex, 1)
+    var card = $('<div>', { 'class': 'card' });
+    var front = $('<div>', { 'class': image });
+    var back = $('<div>', { 'class': 'lfz' });
+    card.append(front);
+    card.append(back);
+    $("#container").append(card);
+  }
+
+}
