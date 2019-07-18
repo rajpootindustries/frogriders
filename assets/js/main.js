852b7c2ce55eb30edf268ea1b94fdf79be4a9e50
James Yamagata <wilsonhj@uci.edu>
2019-07-18 15:26:12 -0700

    created modal window with button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready( initializeApp );
 
 function initializeApp() {
-    // debugger;
+    
     var gameBoard = new Board();
     gameBoard.addPlayer('Warren');
     gameBoard.addPlayer('Elon');
 
     gameBoard.intializeBoard();
+    
+    let modal = new Modal('#modalShadow', "#modalBody", "#modalMessage", "#modalButton" );
+    modal.updateMessage("Player One Dominates!");
+    modal.init();
 
 }
